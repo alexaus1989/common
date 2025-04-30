aa5ad884ed5888647c68bffdf4bb7678a67dd71c
alexaus1989 <46412085+alexaus1989@users.noreply.github.com>
2025-04-30 11:34:55 +0700

    1

--- src/main/kotlin/common/transform/UUIDTransorm.kt
+++ src/main/kotlin/common/transform/UUIDTransorm.kt
@@ -2,11 +2,24 @@ package common.transform
 
 
 class UUIDTransorm {
+    /*
+        public fun addDashesToUUID(uuid: String): String {
+            return uuid.replaceFirst(
+                "(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})".toRegex(),
+                "$1-$2-$3-$4-$5"
+            )
+        }*/
+    fun addDashesToUUID(uuid: String?): String {
+        require(!uuid.isNullOrBlank()) { "UUID string is null or blank" }
 
-    public fun addDashesToUUID(uuid: String): String {
-        return uuid.replaceFirst(
-            "(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})".toRegex(),
-            "$1-$2-$3-$4-$5"
-        )
+        val cleaned = uuid.trim().lowercase()
+        require(cleaned != "null") { "UUID string is literal 'null'" }
+
+        val regex = "(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})".toRegex()
+        require(regex.matches(cleaned)) {
+            "UUID string does not match expected format (32 hex characters)"
+        }
+
+        return regex.replace(cleaned, "$1-$2-$3-$4-$5")
     }
 }
