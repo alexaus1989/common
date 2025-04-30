a1876b24b7d189b094985ff8af722a3f1ac76d47
alexaus1989 <46412085+alexaus1989@users.noreply.github.com>
2025-04-30 13:15:13 +0700

    1

--- src/main/kotlin/common/transform/UUIDTransorm.kt
+++ src/main/kotlin/common/transform/UUIDTransorm.kt
@@ -1,5 +1,7 @@
 package common.transform
 
+import java.util.*
+
 
 class UUIDTransorm {
     /*
@@ -15,11 +17,17 @@ class UUIDTransorm {
         val cleaned = uuid.trim().lowercase()
         require(cleaned != "null") { "UUID string is literal 'null'" }
 
-        val regex = "(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})".toRegex()
-        require(regex.matches(cleaned)) {
-            "UUID string does not match expected format (32 hex characters)"
+        return if (cleaned.contains("-")) {
+            // UUID уже с тире — проверим, валиден ли
+            UUID.fromString(cleaned) // проверка валидности
+            cleaned
+        } else {
+            val regex = "(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})".toRegex()
+            require(regex.matches(cleaned)) {
+                "UUID string does not match expected format (32 hex characters)"
+            }
+            regex.replace(cleaned, "$1-$2-$3-$4-$5")
         }
-
-        return regex.replace(cleaned, "$1-$2-$3-$4-$5")
     }
+
 }
